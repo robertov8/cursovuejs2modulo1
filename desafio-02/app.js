e90affb3b46cd8f0cc8fd551e2ebaca1161ea911
Roberto Ribeiro <robertov82008@gmail.com>
2019-09-01 15:17:12 -0300

    26. Hora de Praticar - Eventos (Resposta)

--- desafio-02/app.js
+++ desafio-02/app.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#desafio',
     data: {
         valor: ''
+    },
+    methods: {
+        showAlert() {
+            alert('showAlert');
+        },
+        changeValue(e) {
+            this.valor = e.target.value;
+        }
     }
-})
+});
