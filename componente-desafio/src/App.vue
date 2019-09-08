f9a98f12d187604fc252dbb47b59ac7ada3e7fe5
Roberto Ribeiro <robertov82008@gmail.com>
2019-09-08 18:44:48 -0300

    108. Hora de Praticar: Organizar Projeto em Componentes (Resposta)

---
+++ componente-desafio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-app>
+        <NavigationDrawer />
+
+        <Toolbar />
+
+        <Content />
+
+        <Footer />
+    </v-app>
+</template>
+
+<script>
+    import NavigationDrawer from "./NavigationDrawer";
+    import Toolbar from "../Toolbar";
+    import Content from "./Content";
+    import Footer from "./Footer";
+
+    export default {
+        components: {
+            Footer,
+            NavigationDrawer,
+            Toolbar,
+            Content
+        }
+    }
+</script>
