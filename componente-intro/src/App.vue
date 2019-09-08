6908c49321f508fb2efebe9766e55b69ac24a284
Roberto Ribeiro <robertov82008@gmail.com>
2019-09-08 16:11:57 -0300

    103. Criar Projeto Usando Vue CLI

---
+++ componente-intro/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Contadores</h1>
+    {{ contador }}
+
+    <div>
+      <button @click="contador++">+</button>
+      <button @click="contador--">-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data() {
+      return {contador: 0};
+    }
+  }
+</script>
+
+<style>
+
+</style>
