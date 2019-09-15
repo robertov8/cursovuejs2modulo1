75f66db4785e0041281872bbb405c5eab95324b8
Roberto Ribeiro <robertov82008@gmail.com>
2019-09-15 16:07:26 -0300

    141. Alternando entre Múltiplos Componentes com Componentes Dinâmicos

--- componentes-avancados/citacoes/src/App.vue
+++ componentes-avancados/citacoes/src/App.vue
@@ -1,30 +1,38 @@
 <template>
-	<div id="app">
-		<Citacoes />
-		<Sobre />
-	</div>
+    <div id="app">
+        <span>
+            <button @click="component = 'Citacoes'">Citações</button>
+            <button @click="component = 'Sobre'">Sobre</button>
+        </span>
+        <Component :is="component" />
+    </div>
 </template>
 
 <script>
-import Citacoes from './components/Citacoes'
-import Sobre from './components/Sobre'
+    import Citacoes from './components/Citacoes'
+    import Sobre from './components/Sobre'
 
-export default {
-	components: { Citacoes, Sobre }
-}
+    export default {
+        components: {Citacoes, Sobre},
+        data() {
+            return {
+                component: 'Citacoes'
+            };
+        }
+    }
 </script>
 
 <style>
-	#app {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-		font-size: 1.6rem;
-	}
+    #app {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        font-size: 1.6rem;
+    }
 
-	button {
+    button {
         outline: none;
         padding: 5px 15px 10px;
         margin: 10px 5px;
