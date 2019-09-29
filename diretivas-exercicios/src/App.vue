33fa49df4a82842c2852b1e847ad0bdb59f46c09
Roberto Ribeiro <robertov82008@gmail.com>
2019-09-29 20:43:22 -0300

    181. Passando Valor para Diretiva Personalizada

--- diretivas-exercicios/src/App.vue
+++ diretivas-exercicios/src/App.vue
@@ -1,32 +1,37 @@
 <template>
-	<div id="app">
-		<h1>Diretivas</h1>
+    <div id="app">
+        <h1>Diretivas</h1>
 
-		<hr>
+        <hr>
 
-		<p v-text="'Usando diretiva v-text'"></p>
-		<p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
+        <p v-text="'Usando diretiva v-text'"></p>
+        <p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
 
-		<hr>
+        <hr>
 
-		<p v-destaque>Usando diretiva personalizada</p>
-	</div>
+        <p v-destaque="'blue'">Usando diretiva personalizada</p>
+        <p v-destaque="cor">Usando diretiva personalizada</p>
+    </div>
 </template>
 
 <script>
-export default {
-	
-}
+    export default {
+        data() {
+            return {
+                cor: 'red'
+            };
+        }
+    };
 </script>
 
 <style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-	font-size: 2.5rem;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+        font-size: 2.5rem;
+    }
 </style>
