fcab971efe26ac3f16238851a78f763742057e66
Roberto Ribeiro <robertov82008@gmail.com>
2019-09-29 21:37:58 -0300

    187. Passando Valores mais Complexos para as Diretivas

--- diretivas-exercicios/src/App.vue
+++ diretivas-exercicios/src/App.vue
@@ -16,10 +16,15 @@
 
         <hr>
 
-        <p v-destaque-local:fundo="'lightgreen'">Usando diretiva personalizada</p>
-        <p v-destaque-local:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
-        <p v-destaque-local="cor">Usando diretiva personalizada</p>
-        <p v-destaque-local.atrasar="'gray'">Usando diretiva personalizada</p>
+        <p v-destaque-local:fundo.alternar="'lightgreen'">Usando diretiva personalizada</p>
+        <p v-destaque-local:fundo.atrasar.alternar="'lightblue'">Usando diretiva personalizada</p>
+        <p v-destaque-local.alternar="cor">Usando diretiva personalizada</p>
+        <p v-destaque-local.atrasar.alternar="'gray'">Usando diretiva personalizada</p>
+
+        <hr>
+
+        <p v-destaque-local.atrasar.alternar="{cor1: 'green', cor2: 'red', atraso: 2000, interval: 200}">Usando diretiva personalizada</p>
+        <p v-destaque-local.atrasar="{cor1: 'green', atraso: 2000}">Usando diretiva personalizada</p>
     </div>
 </template>
 
@@ -28,18 +33,34 @@
         directives: {
             'destaque-local': {
                 bind(el, binding, vnode) {
-                    // el.style.backgroundColor = 'lightgreen';
-                    const color = binding.value || 'lightgreen';
+                    const aplicarCor = cor => {
+                        const color = cor || 'lightgreen';
 
-                    let atraso = 0;
-
-                    if(binding.modifiers['atrasar'])
-                        atraso = 3000;
-
-                    setTimeout(() => {
                         (binding.arg === 'fundo') ?
                             el.style.backgroundColor = color :
                             el.style.color = color;
+                    };
+
+                    let atraso = 0;
+
+                    if(binding.modifiers['atrasar']) atraso = binding.value.atraso;
+
+
+                    const cor1 = binding.value.cor1 || binding.value;
+                    const cor2 = binding.value.cor2;
+                    const interval = binding.value.interval || 1000;
+                    let corAtual = cor1;
+
+                    setTimeout(() => {
+                        if (binding.modifiers['alternar']) {
+                            setInterval(() => {
+                                corAtual = corAtual === cor1 ? cor2 : cor1;
+
+                                aplicarCor(corAtual);
+                            }, interval);
+                        } else {
+                            aplicarCor(cor1);
+                        }
                     }, atraso);
                 }
             }
