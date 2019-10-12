3dd07d7974265aacb7396a9929498249ce877881
Roberto Ribeiro <robertov82008@gmail.com>
2019-10-12 17:42:07 -0300

    234. Criando Formulário

--- http-exercicios/src/App.vue
+++ http-exercicios/src/App.vue
@@ -1,21 +1,58 @@
 <template>
     <div id="app" class="container">
         <h1>HTTP com Axios</h1>
+        
+        <b-card>
+            <b-form-group label="Nome:">
+                <b-form-input
+                    type="text"
+                    size="lg"
+                    v-model="usuario.nome"
+                    placeholder="Informe o Nome" />
+            </b-form-group>
+    
+            <b-form-group label="Email:">
+                <b-form-input
+                        type="email"
+                        size="lg"
+                        v-model="usuario.email"
+                        placeholder="Informe o Email" />
+            </b-form-group>
+            
+            <b-button
+                @click="salvar"
+                size="lg"
+                variant="primary">Salvar</b-button>
+        </b-card>
     </div>
 </template>
 
 <script>
     export default {
-        async created() {
-            console.log('exec!!!');
-            
-            const response = await this.$http.post('usuarios.json', {
-                nome: 'Maria',
-                email: 'maria_maria@gmail.com'
-            });
-    
-            console.log(response);
+        data() {
+            return {
+                usuario: {
+                    nome: '',
+                    email: ''
+                }
+            }
+        },
+        methods: {
+            salvar() {
+                console.log(this.usuario.nome);
+                console.log(this.usuario.email);
+            }
         }
+        // async created() {
+        //     console.log('exec!!!');
+        //
+        //     const response = await this.$http.post('usuarios.json', {
+        //         nome: 'Maria',
+        //         email: 'maria_maria@gmail.com'
+        //     });
+        //
+        //     console.log(response);
+        // }
     };
 </script>
 
