077e4152eaf6744f5c4a431a06bb6370734598db
Roberto Ribeiro <robertov82008@gmail.com>
2019-10-12 19:35:44 -0300

    239. Interceptando Resposta

--- http-exercicios/src/plugins/axios.js
+++ http-exercicios/src/plugins/axios.js
@@ -8,5 +8,15 @@ Vue.use({
     install(Vue) {
         // Vue.prototype.$http = axios;
         Vue.prototype.$http = axios.create({ baseURL });
+        
+        Vue.prototype.$http.interceptors.request.use(request => {
+            console.log(request.method);
+            return request;
+        }, error => Promise.reject(error));
+        
+        Vue.prototype.$http.interceptors.response.use(response => {
+            console.log(response);
+            return response;
+        }, error => Promise.reject(error));
     }
 });
