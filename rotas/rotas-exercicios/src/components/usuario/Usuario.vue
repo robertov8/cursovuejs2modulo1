48ab95561b98c772dac6e8b478a172daeb5cf76f
Roberto Ribeiro <robertov82008@gmail.com>
2019-10-13 12:38:01 -0300

    251. Navegação via Código (Navegação Imperativa)

--- rotas/rotas-exercicios/src/components/usuario/Usuario.vue
+++ rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -1,12 +1,21 @@
 <template>
     <div class="usuario">
         <h2>Usuários</h2>
+    
+        <hr>
+        
+        <button sucesso @click="irParaInicio">Voltar</button>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'Usuario'
+        name: 'Usuario',
+        methods: {
+            irParaInicio() {
+                this.$router.push('/');
+            }
+        }
     };
 </script>
 
